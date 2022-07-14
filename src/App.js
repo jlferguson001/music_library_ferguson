7a82c7a00da7b6ce7bf1090fbac57a4334049de3
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-07-13 19:54:17 -0500

    part 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import { DataContext} from'./context/DataContext';
 
 function App() {
   let [search, setSearch] = useState("");
@@ -37,7 +38,10 @@ function App() {
     <div>
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
+      
     </div>
   );
 }
