5872798c5689ea918c10f51894ac8dc7e43f2f83
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-07-16 11:02:27 -0500

    spinner

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react";
+import "./App.css";
+import { useState, Suspense, useEffect } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import Spinner from "./components/Spinner";
+import { createResource as fetchData } from "./helper";
 
 function App() {
-  let [search, setSearch] = useState("");
-  let [message, setMessage] = useState(
-    "Search for Music! But only Good music!"
-  );
-  let [data, setData] = useState([]);
-
-  const API_URL = "https://itunes.apple.com/search?term=";
+  let [searchTerm, setSearchTerm] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState(null);
 
   useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`;
-        const response = await fetch(API_URL + search);
-        const resData = await response.json();
-        if (resData.results.length > 0) {
-          setData(resData.results);
-        } else {
-          setMessage("Not Found");
-        }
-      };
-      fetchData();
+    if (searchTerm) {
+      document.title = `${searchTerm} Music`;
+      console.log(fetchData(searchTerm));
+      setData(fetchData(searchTerm));
     }
-  }, [search]);
+  }, [searchTerm]);
 
-  //function to pass down to our searchbar passing two arguments one default event object the other represent the search term we will generate by typing in our searchbar
   const handleSearch = (e, term) => {
     e.preventDefault();
-    setSearch(term);
+    setSearchTerm(term);
+  };
+
+  const renderGallery = () => {
+    if (data) {
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      );
+    }
   };
 
   return (
-    <div>
+    <div className="App">
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      {renderGallery()}
     </div>
   );
 }
